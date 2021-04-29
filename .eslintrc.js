cbb79f715d52ea8320821c8f86ebe5e966c0d93c
Alexander Zhelyazov <git@zhlyzv.xyz>
2021-04-29 19:50:38 +0100

    Because who cares about semicolons anyway

--- .eslintrc.js
+++ .eslintrc.js
@@ -94,7 +94,8 @@ module.exports = {
                 singleQuote: true,
                 printWidth: 120,
                 tabWidth: 4,
+                semi: false,
             },
         ],
     },
-};
+}
